9591816a60ead97dbaa0f7bd02ab9095438e6972
Jian Li <jianli@hk.com>
2022-09-01 19:13:41 +0800

    add

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	r.GET("", func(ctx *gin.Context) {
 		log.Println("connectivity test")
 		ctx.JSON(http.StatusOK, gin.H{
-			"message": "Welcome to here!",
+			"message": "Welcome to you!",
 		})
 	})
 
