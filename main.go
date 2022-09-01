40f83caddd89a437f0b0b82a6973be054e6dc83a
Jian Li <jianli@hk.com>
2022-09-01 17:34:21 +0800

    修改路由

--- main.go
+++ main.go
@@ -13,10 +13,10 @@ import (
 func main() {
 	r := gin.Default()
 
-	r.GET("/", func(ctx *gin.Context) {
+	r.GET("", func(ctx *gin.Context) {
 		log.Println("connectivity test")
 		ctx.JSON(http.StatusOK, gin.H{
-			"message": "Welcome to You!",
+			"message": "Welcome to here!",
 		})
 	})
 
